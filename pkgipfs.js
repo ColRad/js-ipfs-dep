508c0442764358d79665963e885a9739e7e87196
Collin Raddatz <collin.raddatz@gmail.com>
2019-02-01 20:05:13 +0100

    First implementation

---
+++ pkgipfs.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+const { exec } = require('pkg');
+
+const targetPlatform = process.platform.replace('darwin', 'macos');
+
+const target = `node${process.version.replace('v', '').split('.')[0]}-${targetPlatform}-x64`;
+console.log(`Compiling IPFS binary for ${target}`);
+const outDir = 'pkg';
+exec(['package.json', '--target', target, '--out-dir', outDir]).then(() => {
+    let nativeModules = ['keccak', 'leveldown', 'rabin', 'secp256k1', 'ursaNative'];
+
+    nativeModules.forEach((m) => {
+        glob(path.join('node_modules', '**', 'Release', `${m}.node`), function (err, files) {
+            if (err) throw err;
+            if (!files || files.length === 0) throw new Error(`Error finding .node file for module ${m}`);
+            fs.copyFileSync(files[0], path.join(outDir, path.basename(files[0])));
+        });
+    });
+}).catch(e => {
+    console.error(e);
+});
